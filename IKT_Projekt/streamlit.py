2c1659d64e06e3de8660d4ce3f6b8e828cada80b
Irina Ukhanova <ukhanovait@gmail.com>
2024-09-15 21:05:13 +0200

    Update streamlit.py

--- IKT_Projekt/streamlit.py
+++ IKT_Projekt/streamlit.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-#from prediction import predict
+import joblib
+from prediction import predict
 
 
 st.title('Spam-Projekt, IKT-SS24')
