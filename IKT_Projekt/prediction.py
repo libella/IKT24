1081d7c2b0875bc9843e5ad2553d9874484da5e3
Irina Ukhanova <ukhanovait@gmail.com>
2024-09-15 20:26:03 +0200

    Update prediction.py

--- IKT_Projekt/prediction.py
+++ IKT_Projekt/prediction.py
@@ -1,5 +1,5 @@
 import joblib
 def predict(data):
-model = joblib.load("spam_model.joblib")
-return model.predict(data)
-from prediction import predict
+    model = joblib.load("spam_model.joblib")
+    return model.predict(data)
+
