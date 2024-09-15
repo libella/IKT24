e08d2b92ab14d581870c6c111e70499d2482f6da
Irina Ukhanova <ukhanovait@gmail.com>
2024-09-15 18:56:10 +0200

    Update streamlit.py

--- IKT_Projekt/streamlit.py
+++ IKT_Projekt/streamlit.py
@@ -5,20 +5,13 @@ import pandas as pd
 
 st.title('Spam-Projekt, IKT-SS24')
 st.subheader('Irina Ukhanova')
-st.header('Thema', divider='rainbow')
-container1 = st.container(border=False)
-container1.write("Klassifikation von Spam-Buchungsanfragen auf einem Immobilienportal")
-st.header('Datensatz', divider='rainbow')
-container2 = st.container(border=False)
-container2.write("Ein unternehmensinterner Datensatz, der Spam-Buchungsanfragen enthält. Dieser Datensatz besteht aus folgenden Merkmalen der Buchungsanfragen: Inhalt der Nachricht, E-Mail.")
 
-st.header('Kurzbeschreibung', divider='rainbow')
-container3 = st.container(border=False)
-container3.write("Das Ziel dieses Projekts ist es, die Sicherheit von Buchungsanfragen auf einem Immobilienportal zu erhöhen, indem anfällige und gefährliche Anfragen automatisch vom System erkannt und blockiert werden. Hierbei sollen ML-Techniken angewendet werden, um legitime Buchungsanfragen von Spam zu unterscheiden. Dies schützt sowohl das Unternehmen als auch die Nutzer vor potenziellen Betrugsversuchen und anderen schädlichen Aktivitäten.")
-st.header('Status Quo', divider='rainbow')
-st.image('IKT_Projekt/img/SPAMprojekt.png', caption='Flussdiagramm der aktuellen Spam-Überprüfungs-Prozesse' )
-
-st.header('Data Processing ', divider='rainbow')
-container4 = st.container(border=False)
-st.page_link("https://huggingface.co/flair/ner-german-large", label="Name Entity Recognition German Large")
+st.subheader('Ist diese Nachricht Spam oder nicht?')
+st.markdown('Probieren Sie dieses ML-Modell aus, um zu prüfen, ob die Nachricht auf den P4G-Portalen als Spam markiert werden sollte.')
+with st.form("my_form"):
+	email = st.text_input("E-Mail")
+	txt = st.text_area("Geben Sie hier die Nachricht an (nicht mehr als 1000 Zeichen):", max_chars=1000)
+	submitted = st.form_submit_button("Spam oder nicht?")
+	if submitted:
+		st.write ("Prüfung läuft")
 
